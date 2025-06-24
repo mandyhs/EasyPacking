4a07d43f962b6ab4184e3420b7521cdc10ffd182
Hsieh, Mandy <mandy.hsieh@intel.com>
2025-06-24 15:47:46 +0800

    update path

--- copy_cce_driver.py
+++ copy_cce_driver.py
@@ -11,10 +11,10 @@ CCE_BUILD_ROOT = config_env.CCE_BUILD_ROOT
 LOCAL_DST_ROOT = config_env.LOCAL_DST_ROOT
 
 PLATFORM_MAP: dict[str, list] = {
-    'MTL':[f'{CCE_BUILD_ROOT}:\\IPU6_MTL_x64\\','MTL'],
-    'LNL':[f'{CCE_BUILD_ROOT}:\\IPU7_LNL_x64\\','LNL'], 
-    'PTL':[f'{CCE_BUILD_ROOT}:\\master_IPU7_PTL_x64\\','PTL'],  
-	'ADL':[f'{CCE_BUILD_ROOT}:\\IPU6_ADL_x64\\','ADL'],
+    'MTL':[f'{CCE_BUILD_ROOT}\\IPU6_MTL_x64\\','MTL'],
+    'LNL':[f'{CCE_BUILD_ROOT}\\IPU7_LNL_x64\\','LNL'], 
+    'PTL':[f'{CCE_BUILD_ROOT}\\master_IPU7_PTL_x64\\','PTL'],  
+	'ADL':[f'{CCE_BUILD_ROOT}\\IPU6_ADL_x64\\','ADL'],
     #Z:\IPU7_LNL_x64\13942_1\Product   
 }
 
--- server_cfg/config_env.py
+++ server_cfg/config_env.py
@@ -1,3 +1,3 @@
-CCE_BUILD_ROOT = "\\\twsimglab005\\camera_drivers"
+CCE_BUILD_ROOT = r"\\twsimglab005\camera_drivers"
 LOCAL_DST_ROOT = ".\\"
 PACK_OUTPUT_ROOT = "D:\\PackOut\\"
